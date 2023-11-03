0bf218e21c5c8a2f951983ca8d2f64016e807a07
Arshaq Ahmed <arshahm2004@gmail.com>
2023-11-03 10:17:18 +0000

    initial commit

---
+++ inorder.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(){
+    int x, y, z;
+    cout << "Enter three integers: " << endl;
+    cin >> x >> y >> z;
+    cout << x << ", " << y << ", " << z << endl;
+return 0;
+}
+
+
+
